839feb314a8665e6f81e658afa0b742d736a5e89
Alex Muntada <alexm@alexm.org>
2018-04-03 13:47:34 +0200

    Allow zero amount investments

--- src/Goteo/Controller/PoolController.php
+++ src/Goteo/Controller/PoolController.php
@@ -87,10 +87,10 @@ class PoolController extends \Goteo\Core\Controller {
         if($login_required) {
 
             // A reward is required here
-            if($amount_original == 0 && !is_null($amount)) {
-                Message::error(Text::get('pool-amount-first'));
-                return $this->redirect('/pool');
-            }
+//            if($amount_original == 0 && !is_null($amount)) {
+//                Message::error(Text::get('pool-amount-first'));
+//                return $this->redirect('/pool');
+//            }
             // A login is required here
             if(!Session::isLogged()) {
 
