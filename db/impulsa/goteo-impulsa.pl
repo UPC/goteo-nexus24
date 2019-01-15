12840ce7f6a2fb3f78221cc0d60a490359860527
Alex Muntada <alexm@alexm.org>
2019-01-15 11:06:29 +0100

    Add documentation and usage

--- db/impulsa/goteo-impulsa.pl
+++ db/impulsa/goteo-impulsa.pl
@@ -1,5 +1,39 @@
 #!/usr/bin/perl -p
 
+=pod
+
+=head1 NAME
+
+goteo-impulsa.pl - Adapt goteo.org mail templates to impulsa.upc.edu
+
+=head1 SYNOPSIS
+
+    # Dump mail templates only with separate INSERT statements
+    mysqldump --opt --skip-extended-insert --skip-quick goteo template template_lang > goteo.sql
+
+    # Review differences
+    perl goteo-impulsa.pl < goteo.sql | diff -u goteo.sql -
+
+    # Commit changes to database
+    perl goteo-impulsa.pl < goteo.sql | mysql goteo
+
+=head1 DESCRIPTION
+
+Replaces strings from Goteo.org mail templates to match the
+purpose of Impulsa UPC adaptation for collaborative projects.
+
+Since there are so many mail templates, changing them manually
+would be a huge task. Thus, we're trying to automate it a bit
+by tweaking the strings in the database.
+
+=head1 CAVEATS
+
+Playing with string replacements in any database is tricky and
+very dangerous. Please, be careful with your regexes and review
+the differences before committing the changes into the database.
+
+=cut
+
 use strict;
 use warnings;
 use utf8::all;
